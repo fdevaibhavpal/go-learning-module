0ceed8909ea5b442573ce748fcf4073e8c7a921b
Vaibhav Pal <vaibhavpal@coredge.io>
2024-07-06 20:45:24 +0530

    mutex example

---
+++ concurrency_examples/mutex_07/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+    "fmt"
+    "sync"
+)
+
+type SafeCounter struct {
+    v   map[string]int
+    mux sync.Mutex
+}
+
+func (c *SafeCounter) Inc(key string) {
+    c.mux.Lock()
+    c.v[key]++
+    c.mux.Unlock()
+}
+
+func (c *SafeCounter) Value(key string) int {
+    c.mux.Lock()
+    defer c.mux.Unlock()
+    return c.v[key]
+}
+
+func main() {
+    c := SafeCounter{v: make(map[string]int)}
+    for i := 0; i < 100; i++ {
+        go c.Inc("number")
+    }
+
+    fmt.Println(c.Value("number"))
+}
